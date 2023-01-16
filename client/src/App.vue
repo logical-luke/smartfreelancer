1b557ddf34432a6b3c6163eef45295474a152a76
logical-luke <lukasz.ziaja@chess.com>
2023-01-16 11:08:45 +0100

    :sparkles: Add draft version of client

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import ProjectsView from "@/components/Projects.vue";
+import Sidebar from "@/components/Sidebar.vue";
+</script>
+
+<template>
+  <div class="">
+
+    <div>
+      <Sidebar />
+      <ProjectsView />
+    </div>
+  </div>
+</template>
+
---
+++ client/src/components/Projects.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mx-auto lg:ml-80">
+    <section class="py-8">
+      <div class="container px-4 mx-auto">
+        <div class="pt-3 px-6 bg-white rounded border-t-4 border-indigo-500 shadow">
+          <div class="flex justify-between items-center pb-3">
+            <h3 class="font-medium">Backlog</h3>
+            <span class="flex justify-center items-center w-6 h-6 rounded bg-indigo-50 text-indigo-500 text-xs">3</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectsView"
+}
+</script>
+
+<style scoped>
+
+</style>
