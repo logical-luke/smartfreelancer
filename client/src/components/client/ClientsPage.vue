99d76743bf1e63af2b7390ab8fcf381b4f928895
logical-luke <lukasz.ziaja@chess.com>
2023-01-22 22:16:27 +0100

    :sparkles: Add new clients button

--- client/src/components/client/ClientsPage.vue
+++ client/src/components/client/ClientsPage.vue
@@ -1,10 +1,15 @@
 <template>
-
+  <div class="mx-auto lg:ml-80">
+    <NewButton go-to="/client/create">Client</NewButton>
+  </div>
 </template>
 
 <script>
+import NewButton from "@/components/ui/NewButton.vue";
+
 export default {
-  name: "ClientsPage"
+  name: "ClientsPage",
+  components: { NewButton }
 };
 </script>
 
