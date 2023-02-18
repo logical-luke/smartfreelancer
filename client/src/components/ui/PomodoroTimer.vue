df68246b4ed23789542d783051b86c174bc5b4cb
logical-luke <lukasz.ziaja@chess.com>
2023-02-18 12:11:58 +0100

    :art: Code style

--- client/src/components/ui/PomodoroTimer.vue
+++ client/src/components/ui/PomodoroTimer.vue
@@ -6,17 +6,25 @@
   >
     🍅
   </button>
-  <overlay-panel
-    ref="op"
-    :show-close-icon="true"
-  >
+  <overlay-panel ref="op" :show-close-icon="true">
     <div class="flex gap-2 flex-col items-center justify-center">
-    <select-button v-model="selectedTime" :options="times" optionLabel="name" />
-    <input-text type="text" class="w-16" v-model="customTime" placeholder="30m" />
-    <button type="button"
-            class="inline-flex text-center items-center w-auto px-3 flex-nowrap py-3 text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 rounded transition duration-200">
-      Start timer
-    </button>
+      <select-button
+        v-model="selectedTime"
+        :options="times"
+        optionLabel="name"
+      />
+      <input-text
+        type="text"
+        class="w-16"
+        v-model="customTime"
+        placeholder="30m"
+      />
+      <button
+        type="button"
+        class="inline-flex text-center items-center w-auto px-3 flex-nowrap py-3 text-sm font-medium text-white bg-indigo-500 hover:bg-indigo-600 rounded transition duration-200"
+      >
+        Start timer
+      </button>
     </div>
   </overlay-panel>
 </template>
@@ -35,16 +43,16 @@ export default {
       times: [
         { name: "5m", code: "5" },
         { name: "10m", code: "10" },
-        { name: "25m", code: "25" }
+        { name: "25m", code: "25" },
       ],
-      customTime: null
+      customTime: null,
     };
   },
   methods: {
     toggle(event) {
       this.$refs.op.toggle(event);
-    }
-  }
+    },
+  },
 };
 </script>
 
