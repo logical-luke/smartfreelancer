b1866ae559a268ef2b6ae0fb861102fd70bd3632
logical-luke <luke@logical-luke.com>
2024-06-18 13:46:26 +0200

    :sparkles: Add file upload component

--- client/src/components/client/ClientForm.vue
+++ client/src/components/client/ClientForm.vue
@@ -1,18 +1,107 @@
 <template>
+  <div>
+    <label class="flex flex-col text-sm font-semibold gap-2" for="name">
+      {{ $t("Name") }}
+      <input-text
+          class="block w-full md:w-1/2 px-4 py-4 text-sm placeholder-gray-500 bg-white border rounded"
+          id="name"
+          name="name"
+          v-model="client.name"
+          placeholder="John Doe"
+      />
+    </label>
+  </div>
 
+  <div class="w-full md:w-1/2">
+    <label class="flex flex-col gap-2 text-sm font-semibold" for="email">{{ $t("Email") }}
+      <input-group>
+        <input-group-addon>
+          <mail-icon/>
+        </input-group-addon>
+        <input-text id="email" name="email" placeholder="john.doe@domain.com"/>
+      </input-group>
+    </label>
+  </div>
+
+  <div class="w-full md:w-1/2">
+    <label class="flex flex-col gap-2 text-sm font-semibold" for="phone">{{ $t("Phone") }}
+      <input-group>
+        <input-group-addon>
+          <phone-icon/>
+        </input-group-addon>
+        <input-text id="phone" name="phone" placeholder="+1 561-555-7689"/>
+      </input-group>
+    </label>
+
+  </div>
+
+  <div class="w-full md:w-1/2">
+    <label class="flex flex-col gap-2 text-sm font-semibold" for="phone">{{ $t("Photo") }}
+      <file-upload mode="basic"
+                   class="inline-flex
+                  shadow
+                  p-4
+                  min-w-40
+                  justify-center
+                  flex-nowrap
+                  items-center
+                  text-sm
+                  font-medium
+                  bg-white
+                  border-2
+                  border-indigo-500
+                  text-black
+                  hover:bg-indigo-500
+                  hover:text-white
+                  rounded-md
+                  disabled:bg-slate-50
+                  disabled:text-slate-500
+                  transition
+                  duration-200"
+                   name="demo[]"
+                   url="/api/upload"
+                   accept="image/*"
+                   :maxFileSize="1000000"
+                   @upload="onUpload"
+                   :auto="true"
+                   :chooseLabel="chooseLabel"
+      />
+    </label>
+
+  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
-import InputText from "primevue/inputtext";
+import {mapState} from "vuex";
+import InputText from 'primevue/inputtext';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
 import MailIcon from "vue-tabler-icons/icons/MailIcon";
+import PhoneIcon from "vue-tabler-icons/icons/PhoneIcon";
+import FileUpload from 'primevue/fileupload';
 
 export default {
   name: "ClientForm",
-  components: { MailIcon, InputText },
-  computed: mapState({
-    client: (state) => state.client.current,
-  }),
+  components: {MailIcon, InputText, InputGroup, InputGroupAddon, PhoneIcon, FileUpload},
+  data() {
+    return {
+      client: {
+        name: "",
+        avatar: "",
+        email: "",
+        phone: ""
+      }
+    }
+  },
+  computed: {
+    chooseLabel() {
+      return this.$t("Browse");
+    }
+  },
+  methods: {
+    onUpload(event) {
+    }
+  }
 };
 </script>
 
--- client/src/views/client/AddClientPage.vue
+++ client/src/views/client/AddClientPage.vue
@@ -6,55 +6,8 @@
   </div>
   <div class="flex container">
     <div class="flex flex-col gap-4 w-full">
-      <div>
-        <label class="flex flex-col text-sm font-medium gap-2" for="name">
-          {{ $t("Name") }}
-          <input-text
-              class="block w-full md:w-1/2 px-4 py-4 text-sm placeholder-gray-500 bg-white border rounded"
-              id="name"
-              name="name"
-              v-model="client.name"
-              placeholder="John Doe"
-          />
-        </label>
-      </div>
 
-      <div class="w-full md:w-1/2">
-        <label class="flex flex-col gap-2 text-sm font-medium" for="email">{{ $t("Email") }}
-          <input-group>
-            <input-group-addon>
-              <mail-icon/>
-            </input-group-addon>
-            <input-text id="email" name="email" placeholder="john.doe@domain.com"/>
-          </input-group>
-        </label>
-      </div>
-
-      <div class="w-full md:w-1/2">
-        <label class="flex flex-col gap-2 text-sm font-medium" for="phone">{{ $t("Phone") }}
-          <input-group>
-            <input-group-addon>
-              <phone-icon/>
-            </input-group-addon>
-            <input-text id="phone" name="phone" placeholder="+1 561-555-7689"/>
-          </input-group>
-        </label>
-
-      </div>
-
-      <div>
-        <label class="flex flex-col gap-2 text-sm font-medium" for="description">
-          {{ $t("Description") }}
-          <textarea
-              class="block w-full md:w-1/2 px-4 py-4 text-sm placeholder-gray-500 bg-white border rounded"
-              id="description"
-              name="description"
-              v-model="client.description"
-              placeholder="Started as small business working on..."
-              rows="5"
-          ></textarea>
-        </label>
-      </div>
+      <client-form props="client" />
 
       <div class="flex gap-4 justify-center md:justify-start w-full md:w-1/2">
         <main-action-button @click="submitForm" class="w-1/2 md:w-auto">
@@ -70,33 +23,20 @@
 
 <script>
 import ClientForm from "@/components/client/ClientForm.vue";
-import {mapState} from "vuex";
-import SubmitButton from "@/components/SubmitButton.vue";
-import SquarePlusIcon from "vue-tabler-icons/icons/SquarePlusIcon";
 import ActionButton from "@/components/ActionButton.vue";
-import InputText from 'primevue/inputtext';
-import InputGroup from 'primevue/inputgroup';
-import InputGroupAddon from 'primevue/inputgroupaddon';
-import MailIcon from "vue-tabler-icons/icons/MailIcon";
-import PhoneIcon from "vue-tabler-icons/icons/PhoneIcon";
 import MainActionButton from "@/components/MainActionButton.vue";
 
 export default {
   name: "AddClientPage",
   components: {
     MainActionButton,
-    PhoneIcon, ActionButton, SquarePlusIcon, SubmitButton, ClientForm, InputText, InputGroupAddon, InputGroup, MailIcon
+    ActionButton,
+    ClientForm,
   },
   data() {
     return {
       clientsPageRoute: {
         name: "ClientsPage"
-      },
-      client: {
-        name: "",
-        description: "",
-        email: "",
-        phone: ""
       }
     }
   },
