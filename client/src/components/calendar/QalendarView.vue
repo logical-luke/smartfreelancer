fccdba8c08035b6090265d675f2eee621994dde9
logical-luke <luke@logical-luke.com>
2024-09-30 12:12:30 +0200

    :clown_face: Add Qalendar component to CalendarPage
    Integrated Qalendar into the CalendarPage for enhanced calendar functionality. Updated package.json and package-lock.json to include all necessary dependencies for Qalendar.

---
+++ client/src/components/calendar/QalendarView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Qalendar } from 'qalendar';
+import 'qalendar/dist/style.css';
+
+const events = ref([
+  {
+    title: "Advanced algebra",
+    with: "Chandler Bing",
+    time: { start: "2022-05-16 12:05", end: "2022-05-16 13:35" },
+    color: "yellow",
+    isEditable: true,
+    id: "753944708f0f",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+  },
+  {
+    title: "Ralph on holiday",
+    with: "Rachel Greene",
+    time: { start: "2022-05-10", end: "2022-05-22" },
+    color: "green",
+    isEditable: true,
+    id: "5602b6f589fc"
+  }
+]);
+
+const config = ref({
+  // Add your configuration here
+});
+</script>
+
+<template>
+  <Qalendar :events="events" :config="config" />
+</template>
+
+<style scoped>
+@import "qalendar/dist/style.css";
+</style>
--- client/src/views/calendar/CalendarPage.vue
+++ client/src/views/calendar/CalendarPage.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import PageHeader from "@/components/page/PageHeader.vue";
+import QalendarView from "@/components/calendar/QalendarView.vue";
 </script>
 
 <template>
-  <PageHeader title="Calendar" icon="pi-calendar" />
+  <PageHeader title="Calendar" icon="pi-calendar"/>
+  <div class="flex container">
+    <QalendarView/>
+  </div>
 </template>
